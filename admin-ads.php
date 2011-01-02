3ced766ce30be450acce8cd1c886087537306573
moha <moha@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-01-02 12:19:39 +0000

    - 3.1+ compatibility update
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/admin-ads@1692 f53fe46e-8c02-459b-9fdd-93e75771b278

--- admin-ads.php
+++ admin-ads.php
@@ -1,11 +1,12 @@
 <?php
 /*
 Plugin Name: Admin Ads
-Plugin URI: 
-Description:
-Author: Andrew Billits
-Version: 1.0.8
-Author URI:
+Plugin URI: http://premium.wpmudev.org/project/admin-ads
+Description: Display ads in admin dashboard
+Author: S H Mohanjith (Incsub), Andrew Billits (Incsub)
+Version: 1.0.9
+Network: true
+Author URI: http://premium.wpmudev.org
 WDP ID: 6
 */
 
@@ -26,11 +27,23 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
+global $admin_ads_settings_page, $admin_ads_settings_page_long;
+
+if ( version_compare($wp_version, '3.0.9', '>') ) {
+	$admin_ads_settings_page = 'settings.php';
+	$admin_ads_settings_page_long = 'network/settings.php';
+} else {
+	$admin_ads_settings_page = 'ms-admin.php';
+	$admin_ads_settings_page_long = 'ms-admin.php';
+}
+
 //------------------------------------------------------------------------//
 //---Hook-----------------------------------------------------------------//
 //------------------------------------------------------------------------//
 add_action('admin_menu', 'admin_ads_plug_pages');
+add_action('network_admin_menu', 'admin_ads_plug_pages');
 add_action('admin_notices', 'admin_ads_output');
+add_action('network_admin_notices', 'admin_ads_output');
 //------------------------------------------------------------------------//
 //---Functions------------------------------------------------------------//
 //------------------------------------------------------------------------//
@@ -43,9 +56,16 @@ function admin_ads_output() {
 }
 
 function admin_ads_plug_pages() {
-	global $wpdb, $wp_roles, $current_user;
-	if ( is_site_admin() ) {
-		add_submenu_page('ms-admin.php', 'Admin Ads', 'Admin Ads', 10, 'admin-ads', 'admin_ads_page_main_output');
+	global $wpdb, $wp_roles, $current_user, $wp_version;
+	
+	if ( version_compare($wp_version, '3.0.9', '>') ) {
+		if ( is_network_admin() ) {
+			add_submenu_page($admin_ads_settings_page, 'Admin Ads', 'Admin Ads', 10, 'admin-ads', 'admin_ads_page_main_output');
+		}
+	} else {
+		if ( is_site_admin() ) {
+			add_submenu_page($admin_ads_settings_page, 'Admin Ads', 'Admin Ads', 10, 'admin-ads', 'admin_ads_page_main_output');
+		}
 	}
 }
 
@@ -73,7 +93,7 @@ function admin_ads_page_main_output() {
 			}
 			?>
 			<h2><?php _e('Admin Ads') ?></h2>
-            <form method="post" action="ms-admin.php?page=admin-ads&action=update">
+            <form method="post" action="<?php print $admin_ads_settings_page_long; ?>?page=admin-ads&action=update">
             <table class="form-table">
             <tr valign="top">
             <th scope="row"><?php _e('Ad Code') ?></th>
@@ -96,7 +116,7 @@ function admin_ads_page_main_output() {
 				update_site_option( "admin_ads_data", "empty" );
 				echo "
 				<SCRIPT LANGUAGE='JavaScript'>
-				window.location='ms-admin.php?page=admin-ads&updated=true&updatedmsg=" . urlencode(__('Settings cleared.')) . "';
+				window.location='{$admin_ads_settings_page}?page=admin-ads&updated=true&updatedmsg=" . urlencode(__('Settings cleared.')) . "';
 				</script>
 				";
 			} else {
@@ -107,7 +127,7 @@ function admin_ads_page_main_output() {
 				update_site_option( "admin_ads_data", stripslashes($admin_ads_data) );
 				echo "
 				<SCRIPT LANGUAGE='JavaScript'>
-				window.location='ms-admin.php?page=admin-ads&updated=true&updatedmsg=" . urlencode(__('Settings saved.')) . "';
+				window.location='{$admin_ads_settings_page}?page=admin-ads&updated=true&updatedmsg=" . urlencode(__('Settings saved.')) . "';
 				</script>
 				";
 			}
@@ -120,4 +140,12 @@ function admin_ads_page_main_output() {
 	echo '</div>';
 }
 
-?>
+if ( !function_exists( 'wdp_un_check' ) ) {
+	add_action( 'admin_notices', 'wdp_un_check', 5 );
+	add_action( 'network_admin_notices', 'wdp_un_check', 5 );
+
+	function wdp_un_check() {
+		if ( !class_exists( 'WPMUDEV_Update_Notifications' ) && current_user_can( 'edit_users' ) )
+			echo '<div class="error fade"><p>' . __('Please install the latest version of <a href="http://premium.wpmudev.org/project/update-notifications/" title="Download Now &raquo;">our free Update Notifications plugin</a> which helps you stay up-to-date with the most stable, secure versions of WPMU DEV themes and plugins. <a href="http://premium.wpmudev.org/wpmu-dev/update-notifications-plugin-information/">More information &raquo;</a>', 'wpmudev') . '</a></p></div>';
+	}
+}
