1105bccdafcbd6688bdcb108dc5b77ccebc83e57
moha <moha@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-01-19 22:06:15 +0000

    is_site_admin => is_super_admin
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/admin-ads@2380 f53fe46e-8c02-459b-9fdd-93e75771b278

--- admin-ads.php
+++ admin-ads.php
@@ -71,7 +71,7 @@ function admin_ads_plug_pages() {
 			add_submenu_page($admin_ads_settings_page, __('Admin Ads', 'admin_ads'), __('Admin Ads', 'admin_ads'), 10, 'admin-ads', 'admin_ads_page_main_output');
 		}
 	} else {
-		if ( is_site_admin() ) {
+		if ( is_super_admin() ) {
 			add_submenu_page($admin_ads_settings_page, __('Admin Ads', 'admin_ads'), __('Admin Ads', 'admin_ads'), 10, 'admin-ads', 'admin_ads_page_main_output');
 		}
 	}
